0da14172d82fd6a25eb5d7d7a751ccdf4e094c42
Heather Haack <63765785+heatherhaack@users.noreply.github.com>
2020-06-01 09:17:52 -0700

    adds root routes, adds controller routes to server.js

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
+//root routes
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+// artist route
+app.use("/artists", controllers.artist);
+// song route
+app.use("/songs", controllers.song);
+//user route
+app.use("/users", controllers.user);
+
 //bind server
 app.listen(PORT, function(){
     console.log(`Server is running on http://localhost:${PORT}`)
