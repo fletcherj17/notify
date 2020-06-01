4b8eb702769ac962f7aedbce2239a4d79d76d5a2
fletcherj17 <fletcherejones@gmail.com>
2020-06-01 12:56:00 -0400

    preliminarily finished models

--- models/Song.js
+++ models/Song.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const songSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    artist: {type: String, required: true},
+    link: {type: String, required: true, unique: true}
+});
+
+const Song = mongoose.model('Song', songSchema);
+
+module.exports = Song;
