8bc23396d3f7b7fcd197bc0d99c498dc23a82a44
Heather Haack <63765785+heatherhaack@users.noreply.github.com>
2020-06-01 16:37:42 -0700

    adds route to songs show page

--- controllers/song.js
+++ controllers/song.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get('/', (req,res)=>{
+router.get('/', (req,res)=> {
     db.Song.find({}, function(err, allSongs ){
         if(err){
             console.log(err);
@@ -14,9 +14,37 @@ router.get('/', (req,res)=>{
     });
 });
 
-// route for new song
-router.get("/new", function (req, res) {
+// new route
+router.get("/new", (req, res)=> {
     res.render("songs/new");
 });
+
+// create route
+router.post("/", (req,res)=> {
+    db.Song.create(req.body, function (err, createdSong) {
+        if (err) {
+        console.log(err);
+        res.send({ message: "Internal Server Error" });
+        } else {
+        res.redirect("/songs");
+        }
+    });
+});
+
+//show route
+router.get("/:id", (req,res)=> {
+    db.Song.findById(req.params.id, (err, foundSong) =>{
+        if(err){
+        console.log(err);
+        res.send({ message: "Internal Server Error" });
+        } else {
+        const context = {song: foundSong}
+        res.render("songs/show", context);
+        }
+    });
+});
+
+
+
 
 module.exports = router;
