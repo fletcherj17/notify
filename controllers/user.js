48dc6e5841b02b60f51f2c853dfd87ecf4202ba0
fletcherj17 <fletcherejones@gmail.com>
2020-06-03 13:42:52 -0400

    created signup route

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-
+const bcrypt = require('bcryptjs');
 
 // root route
 router.get('/', (req,res)=>{
@@ -14,12 +14,22 @@ router.get('/signup', (req, res)=>{
 })
 
 // register post
-router.post('/signup', async (req, res)=>{})
-    //access req.body
-    //search db for user with existing email
-    //if found , send error
-    //if not, we hash password and create user
-    //redirect to login
+router.post("/signup", async function (req, res) {
+    try {
+      // access the req.body
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+      // create user with req.body and hash password
+    const newUser = await db.User.create(req.body);
+      // redirect to login
+    console.log(newUser)
+    res.redirect("/login");
+    } catch (err) {
+    res.send({ message: "Internal Server Error", error: err });
+    }
+});
+
 
 // login form
 router.get('/login', (req, res)=>{
